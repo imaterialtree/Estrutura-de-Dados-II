542c8165d21f98b82edc4808f7182b0595078e93
Naoki Rafael Miura <119423433+imaterialtree@users.noreply.github.com>
2023-03-24 11:38:43 -0300

    Fim da aula

--- Estrutura de Dados II/src/hash/Aula2.java
+++ Estrutura de Dados II/src/hash/Aula2.java
@@ -1,2 +1,31 @@
-package hash;public class Aula2 {
+package hash;
+
+import java.util.HashMap;
+
+public class Aula2 {
+    public static void main(String[] args) {
+        HashMap<String, String> tabela = new HashMap<>();
+
+        tabela.put("355", "Takeshi");
+        tabela.put("415", "Gearle");
+        tabela.put("777", "Everton");
+        tabela.put("888", "Hendrew");
+        tabela.put("954", "Sara");
+        tabela.put("444", "Naoki");
+        tabela.put("514", "Gean");
+        tabela.put("1325", "Gean");
+
+        for (String key : tabela.keySet()) {
+            System.out.println(myHash(key)+" \t"+key+" \t"+tabela.get(key));
+        }
+    }
+    static int myHash(String key) {
+        int[] primos = new int[] {2, 3, 5, 7, 11, 13, 17, 19};
+        int hash = 0;
+        for (int i=0; i<key.length(); i++){
+            int n1 = Integer.parseInt(key.substring(i, i+1));
+            hash += n1*primos[i];
+        }
+        return hash;
+    }
 }
---
+++ Estrutura de Dados II/src/hash/Exercicio.java
@@ -0,0 +1,18 @@
+/*
+1. Crie um programa que trate colisão de forma externa para nome entre 3 e 8 caracteres
+    adicionar nomes
+    excluir nomes
+    buscar se nome existe
+
+2. Colisão dividida
+
+3. Colisão sequencial
+
+ */
+package hash;
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+    }
+}
