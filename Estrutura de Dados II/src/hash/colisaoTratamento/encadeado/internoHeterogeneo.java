bdd7f20ee06f3ab9f7f7a410b19ae0a9880c61e1
Naoki Rafael Miura <119423433+imaterialtree@users.noreply.github.com>
2023-03-31 11:26:44 -0300

    aula4

---
+++ Estrutura de Dados II/src/hash/colisaoTratamento/encadeado/internoHeterogeneo.java
@@ -0,0 +1,19 @@
+package hash.colisaoTratamento.encadeado;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class internoHeterogeneo {
+    public static void main(String[] args) {
+        Map<String, Integer> meuHash = new HashMap<>();
+        int[] encadeamento =
+        int p, s, m, n;
+
+
+        }
+    public static Map addItem(Map map, int num) {
+
+    }
+}
